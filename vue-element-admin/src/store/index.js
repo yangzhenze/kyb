973439dde9519168228f61fb6d38fb0356e42d28
466636298@qq.com <466636298@qq.com>
2020-05-21 22:05:32 +0800

    添加dao代码生成工具类

--- vue-element-admin/src/store/index.js
+++ vue-element-admin/src/store/index.js
@@ -6,6 +6,8 @@ import tagsView from './modules/tagsView'
 import user from './modules/user'
 import getters from './getters'
 
+// Vuex 的状态存储是响应式的。当 Vue 组件从 store 中读取状态的时候，若 store 中的状态发生变化，那么相应的组件也会相应地得到高效更新
+
 Vue.use(Vuex)
 
 const store = new Vuex.Store({
